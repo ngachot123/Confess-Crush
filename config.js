63430a11ce03042c9a644d529a91d6fda69c0e2b
ngachot123 <91784391+ngachot123@users.noreply.github.com>
2021-10-02 18:35:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Đế chế VAnh xinh",
+    introTitle: 'đồ xấu xí',
+    introDesc: `các cậu ơi cho mình chơi cùng nha 
+    xấu xí vậy mà cũng đòi chơi cùng sao
+    biến đi cái đồ xấu xí 
+    mình xin lỗi,
+    btnIntro: '^^hehe^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'không nha',
+    btnNo: 'đéo',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại không yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'vanhvanh quá xinh không yêu nổi',
+    mess: 'cũng biết điều đấy heheheehhehe',
+    messDesc: 'để tạ lỗi mua cho ta ip13pm',
+    btnAccept: 'quá dễ ẹc đưa stk hohohohohoh',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.messenger.com/t/100030164896820/. VD: https://m.me/nam.nodemy
 }
