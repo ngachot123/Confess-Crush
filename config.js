f90bad7f92e76a7ce1867dab739409dcf5ae52b5
ngachot123 <91784391+ngachot123@users.noreply.github.com>
2021-10-02 18:21:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introTitle: 'Minhtrietthoitha',
+    introDesc: `hello con chóa háp pi 1 năm hehe `,
+    btnIntro: '^^bấm vào đây nếu chối^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'bởi vì vanhvanh xinh nhất thế giới chứ còn gì vậy mà cũng hỏi',
+    mess: 'Mình biết mà 🥰. khỏi lói',
+    messDesc: 'mua cho ta ip13',
+    btnAccept: 'dăm ba cái đồng lẻ',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //https://www.messenger.com/t/100030164896820/. VD: https://m.me/nam.nodemy
 }
